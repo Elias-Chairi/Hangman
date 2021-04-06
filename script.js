6fdd30ac75106264ba6d66a39f53821b5b4039db
Elias <eliaschairi@icloud.com>
2021-04-06 15:00:23 +0200

    It now counts errors

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ start_inputs[1].addEventListener("focus", delete_text);
 
 start_button.addEventListener("click", edit_check);
 
+let theWord;
+let guessing_charcater;
+let wrong_counter = 1;
 
 function delete_text(e) {
   e.target.value = "";
   e.target.removeEventListener("focus", delete_text);
 }
+
 
 function edit_check() {
   if (start_inputs[1].value == "The word ..." ||
@@ -21,6 +25,7 @@ function edit_check() {
         start_inputs[0].value == ""){
       start_inputs[0].value = "no hint";
     }
+    theWord = start_inputs[1].value;
     start_game();
   }
 }
@@ -30,12 +35,13 @@ function start_game() {
   document.querySelector("img").src = "bilder/steg1.png";
   document.querySelector("h2").textContent = "Hint: " + start_inputs[0].value;
   document.querySelector("div > div").innerHTML =
-  "<input value='Letter'> <button>Guess</button>";
+  "<input value='Letter' maxlength='1'> <button>Guess</button>";
   let  guess_button = document.querySelector("button");
   guess_button.style.width = "70px";
   guess_button.style.height = "50px";
   guess_button.style.fontSize = "20px";
   guess_button.style.margin = "0px";
+  guess_button.addEventListener("click", guess);
 
   let  guess_input = document.querySelector("input");
   guess_input.style.margin = "0px";
@@ -43,4 +49,36 @@ function start_game() {
   guess_input.style.height = "30px";
   guess_input.style.fontSize = "20px";
   guess_input.addEventListener("focus", delete_text);
+  guess_input.addEventListener("keypress", function enter (e) {
+    if (e.key == "Enter"){guess()}
+  });
+}
+
+
+function guess() {
+  guessing_charcater = document.querySelector("input").value;
+
+  if (theWord.indexOf(guessing_charcater) != -1){
+    correct(guessing_charcater);
+  } else {
+    wrong(guessing_charcater);
+  }
+}
+
+
+function correct(e) {
+  console.log(e + " was correct");
+}
+
+
+function wrong(e) {
+  console.log(e + " was wrong");
+  wrong_counter++;
+  document.querySelector("img").src = "bilder/steg"+wrong_counter+".png";
+  if (wrong_counter == 8){
+    document.querySelector("h2").textContent = "You lost!"
+    document.querySelector("h2").style.color = "red";
+    document.querySelector("div > div").innerHTML = "";
+  }
+
 }
