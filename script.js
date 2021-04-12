1b5cc51c5fb615b1400a0dea705e9f5318aeea4e
Elias <eliaschairi@icloud.com>
2021-04-12 10:38:31 +0200

    You can now guess characters!

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ function delete_text(e) {
 
 function edit_check() {
   if (start_inputs[1].value == "The word ..." ||
-      start_inputs[1].value == ""){
-    alert("You must type a word");
+      start_inputs[1].value == "" ||
+      start_inputs[1].value.charAt(0) == " "){
+    alert("You must type a word, and it cannot start with a space");
   } else {
     if (start_inputs[0].value == "hint ..." ||
         start_inputs[0].value == ""){
@@ -35,7 +36,22 @@ function start_game() {
   document.querySelector("img").src = "bilder/steg1.png";
   document.querySelector("h2").textContent = "Hint: " + start_inputs[0].value;
   document.querySelector("div > div").innerHTML =
-  "<input value='Letter' maxlength='1'> <button>Guess</button>";
+  "<ul id='charcter_list'>" +
+    "<li>_</li>" +
+  "</ul>" +
+  "<input value='Letter' maxlength='1'>" +
+  "<button>Guess</button>";
+
+  for (i = 1; i < theWord.length; i++){
+    let list_item = document.createElement("li");
+    if (theWord.charAt(i) == " "){
+      list_item.appendChild(document.createTextNode("\xa0\xa0\xa0"))
+    } else {
+      list_item.appendChild(document.createTextNode("_"))
+    }
+    document.getElementById("charcter_list").appendChild(list_item);
+  }
+
   let  guess_button = document.querySelector("button");
   guess_button.style.width = "70px";
   guess_button.style.height = "50px";
@@ -68,6 +84,13 @@ function guess() {
 
 function correct(e) {
   console.log(e + " was correct");
+  let character_list = document.querySelectorAll("li");
+
+  for (i = 0; i < theWord.length; i++){
+    if (e == theWord.charAt(i)){
+      character_list[i].textContent = e;
+    }
+  }
 }
 
 
@@ -80,5 +103,4 @@ function wrong(e) {
     document.querySelector("h2").style.color = "red";
     document.querySelector("div > div").innerHTML = "";
   }
-
 }
